4b9066fed493dab6f65fa25388e13ca15bcf679c
Ahmad_Mrf <44136738+AhmadMrf@users.noreply.github.com>
2023-05-03 12:05:43 +0330

    fix layout and responsive

--- src/component/home.js
+++ src/component/home.js
@@ -2,7 +2,7 @@ const home = () => {
   return /*html*/ `
     <section class="max-w-[1920px] mx-auto relative z-10 h-full sm:px-6">
       
-    <div class="absolute w-10/12 sm:top-1/2 top-1/3 left-1/2 -translate-x-1/2 translate-y-1/2 group-[.explore]:text-to-up transition-all duration-700 ease-out flex flex-col items-center gap-12 -mt-[min(20rem,calc(20_*_5vw))]">
+    <div class="absolute w-10/12 sm:top-1/2 top-[45%] left-1/2 -translate-x-1/2 translate-y-1/2 group-[.explore]:text-to-up transition-all duration-700 ease-out flex flex-col items-center gap-12 landscape:-mt-[min(15rem,calc(10_*_5vw))] -mt-[min(20rem,calc(20_*_5vw))]">
           <div class="flex flex-col gap-4">
             <h1 class="text-5xl font-bold text-center">welcome to space-x</h1>
             <h3 class="text-lg opacity-50 text-center">
@@ -24,21 +24,21 @@ const home = () => {
           </a>
         </div>
 
-        <div class="w-10/12 sm:max-w-fit absolute sm:top-1/2 top-1/3 left-1/2 -translate-x-1/2 translate-y-1/2 opacity-0 invisible transition-all duration-700 ease-out sm:min-w-[30rem] sm:group-[.proceed.explore]:text-to-right group-[.explore]:text-to-button sm:group-[.explore]:text-to-left  flex flex-col items-center sm:basis-1/2 gap-12 -mt-[min(20rem,calc(20_*_5vw))]">
+        <div class="w-10/12 sm:max-w-fit absolute sm:top-1/2 top-1/3 left-1/2 -translate-x-1/2 translate-y-1/2 opacity-0 invisible transition-all duration-700 ease-out sm:min-w-[30rem] sm:group-[.proceed.explore]:text-to-right group-[.explore]:text-to-button sm:group-[.explore]:text-to-left  flex flex-col sm:basis-1/2 gap-12 landscape:-mt-[min(15rem,calc(10_*_5vw))] -mt-[min(20rem,calc(20_*_5vw))]">
           <div class="flex flex-col gap-4">
           <div class="grid">
-          <h2 class="group-[.proceed]:opacity-0 opacity-100 duration-700 transition-opacity col-start-1 row-start-1 text-3xl font-bold text-center">plan your family trip into space.</h2>
-          <h2 class="group-[.proceed]:opacity-100 opacity-0 duration-700 transition-opacity  col-start-1 row-start-1 text-3xl font-bold text-center">check out the available trips.</h2>
+          <h2 class="group-[.proceed]:opacity-0 opacity-100 duration-700 transition-opacity col-start-1 row-start-1 text-3xl font-bold text-center sm:text-left">plan your family trip into space.</h2>
+          <h2 class="group-[.proceed]:opacity-100 opacity-0 duration-700 transition-opacity  col-start-1 row-start-1 text-3xl font-bold text-center sm:text-left">check out the available trips.</h2>
           </div>
-          <h3 class="text-lg opacity-50 text-center">
+          <h3 class="text-lg opacity-50 text-center sm:text-left">
               we assure you that is going to be an amazing journey 
             </h3>
           </div>
-          <div class=" group-[.proceed]:flex-col items-center sm:group-[.proceed]:flex-row flex flex-col gap-4">
+          <div class=" items-center sm:items-start sm:group-[.proceed]:flex-row flex flex-col gap-4">
           
           <button
           id="proceed"
-          class=" hover:opacity-70 transition-opacity border border-white px-6 py-2 rounded-md text-2xl capitalize
+          class=" hover:opacity-70 transition-opacity border border-white sm:px-6 sm:py-2 sm:text-2xl px-4 py-1 rounded-md text-xl capitalize
           "
           >
           proceed
@@ -46,9 +46,9 @@ const home = () => {
           
           <a class="group-[.proceed]:opacity-100 group-[.proceed]:visible group-[.proceed]:h-auto h-0 transition-opacity duration-700 invisible opacity-0" href="#about">
           <button
-          class=" hover:opacity-70 border border-white px-6 py-2 rounded-md text-2xl capitalize
+          class=" hover:opacity-70 border border-white sm:px-6 sm:py-2 sm:text-2xl px-4 py-1 rounded-md text-xl capitalize
           "
-          >check out
+          >home
           </button>
           </a>
           </div>
