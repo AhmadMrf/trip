5cb5bb91de3c268a03aee933d3ee7dc4377f6613
Ahmad_Mrf <44136738+AhmadMrf@users.noreply.github.com>
2023-05-03 12:08:59 +0330

    fix layout and responsive

--- assets/index-d3084719.js
+++ assets/index-d3084719.js
@@ -1,7 +1,7 @@
 (function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&l(r)}).observe(document,{childList:!0,subtree:!0});function c(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function l(e){if(e.ep)return;e.ep=!0;const t=c(e);fetch(e.href,t)}})();const n=()=>`
     <section class="max-w-[1920px] mx-auto relative z-10 h-full sm:px-6">
       
-    <div class="absolute w-10/12 sm:top-1/2 top-1/3 left-1/2 -translate-x-1/2 translate-y-1/2 group-[.explore]:text-to-up transition-all duration-700 ease-out flex flex-col items-center gap-12 -mt-[min(20rem,calc(20_*_5vw))]">
+    <div class="absolute w-10/12 sm:top-1/2 top-[45%] left-1/2 -translate-x-1/2 translate-y-1/2 group-[.explore]:text-to-up transition-all duration-700 ease-out flex flex-col items-center gap-12 landscape:-mt-[min(15rem,calc(10_*_5vw))] -mt-[min(20rem,calc(20_*_5vw))]">
           <div class="flex flex-col gap-4">
             <h1 class="text-5xl font-bold text-center">welcome to space-x</h1>
             <h3 class="text-lg opacity-50 text-center">
@@ -23,21 +23,21 @@
           </a>
         </div>
 
-        <div class="w-10/12 sm:max-w-fit absolute sm:top-1/2 top-1/3 left-1/2 -translate-x-1/2 translate-y-1/2 opacity-0 invisible transition-all duration-700 ease-out sm:min-w-[30rem] sm:group-[.proceed.explore]:text-to-right group-[.explore]:text-to-button sm:group-[.explore]:text-to-left  flex flex-col items-center sm:basis-1/2 gap-12 -mt-[min(20rem,calc(20_*_5vw))]">
+        <div class="w-10/12 sm:max-w-fit absolute sm:top-1/2 top-1/3 left-1/2 -translate-x-1/2 translate-y-1/2 opacity-0 invisible transition-all duration-700 ease-out sm:min-w-[30rem] sm:group-[.proceed.explore]:text-to-right group-[.explore]:text-to-button sm:group-[.explore]:text-to-left  flex flex-col sm:basis-1/2 gap-12 landscape:-mt-[min(15rem,calc(10_*_5vw))] -mt-[min(20rem,calc(20_*_5vw))]">
           <div class="flex flex-col gap-4">
           <div class="grid">
-          <h2 class="group-[.proceed]:opacity-0 opacity-100 duration-700 transition-opacity col-start-1 row-start-1 text-3xl font-bold text-center">plan your family trip into space.</h2>
-          <h2 class="group-[.proceed]:opacity-100 opacity-0 duration-700 transition-opacity  col-start-1 row-start-1 text-3xl font-bold text-center">check out the available trips.</h2>
+          <h2 class="group-[.proceed]:opacity-0 opacity-100 duration-700 transition-opacity col-start-1 row-start-1 text-3xl font-bold text-center sm:text-left">plan your family trip into space.</h2>
+          <h2 class="group-[.proceed]:opacity-100 opacity-0 duration-700 transition-opacity  col-start-1 row-start-1 text-3xl font-bold text-center sm:text-left">check out the available trips.</h2>
           </div>
-          <h3 class="text-lg opacity-50 text-center">
+          <h3 class="text-lg opacity-50 text-center sm:text-left">
               we assure you that is going to be an amazing journey 
             </h3>
           </div>
-          <div class=" group-[.proceed]:flex-col items-center sm:group-[.proceed]:flex-row flex flex-col gap-4">
+          <div class=" items-center sm:items-start sm:group-[.proceed]:flex-row flex flex-col gap-4">
           
           <button
           id="proceed"
-          class=" hover:opacity-70 transition-opacity border border-white px-6 py-2 rounded-md text-2xl capitalize
+          class=" hover:opacity-70 transition-opacity border border-white sm:px-6 sm:py-2 sm:text-2xl px-4 py-1 rounded-md text-xl capitalize
           "
           >
           proceed
@@ -45,9 +45,9 @@
           
           <a class="group-[.proceed]:opacity-100 group-[.proceed]:visible group-[.proceed]:h-auto h-0 transition-opacity duration-700 invisible opacity-0" href="#about">
           <button
-          class=" hover:opacity-70 border border-white px-6 py-2 rounded-md text-2xl capitalize
+          class=" hover:opacity-70 border border-white sm:px-6 sm:py-2 sm:text-2xl px-4 py-1 rounded-md text-xl capitalize
           "
-          >check out
+          >home
           </button>
           </a>
           </div>
@@ -100,9 +100,9 @@
           </li>
         </ul>
       </nav>
-    `,u=()=>`
+    `,d=()=>`
   <main class="group relative h-screen overflow-hidden">
       ${p()}
       ${n()}
     </main>
-    `,i=document.body;i.innerHTML=u();location.hash="";const a=i.querySelector("main"),d=a.querySelector("#proceed"),h=s=>{const o=new URL(s.newURL).hash;a.classList.toggle("home",o==="#home"),a.classList.toggle("explore",o==="#explore"),a.classList.remove("proceed")};d.addEventListener("click",()=>{a.classList.toggle("proceed")});window.addEventListener("hashchange",h);
+    `,i=document.body;i.innerHTML=d();location.hash="";const a=i.querySelector("main"),u=a.querySelector("#proceed"),h=s=>{const o=new URL(s.newURL).hash;a.classList.toggle("home",o==="#home"),a.classList.toggle("explore",o==="#explore"),a.classList.remove("proceed")};u.addEventListener("click",()=>{a.classList.toggle("proceed")});window.addEventListener("hashchange",h);
